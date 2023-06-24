ae7eb18bcee4b79ca4c99bae42b0fd57fb3a2494
Sree Chandu <54301407+sree9797@users.noreply.github.com>
2023-06-24 12:30:30 +0530

    LWC to Main branch

---
+++ .sfdx/typings/lwc/apex/ElavonTabTaskController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/ElavonTabTaskController.getRecordTypeDetails" {
+  export default function getRecordTypeDetails(param: {recTypeId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/ElavonTabTaskController.getRecordTypeId" {
+  export default function getRecordTypeId(param: {name: any}): Promise<any>;
+}
